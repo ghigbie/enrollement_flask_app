54bef3cf399c10e47889305dc545eb138ece6275
ghigbie <gehigbie@gmail.com>
2020-05-15 19:37:19 -0700

    Add new routes

--- application/routes.py
+++ application/routes.py
@@ -6,3 +6,15 @@ from flask import render_template
 @app.route("/index")
 def index():
     return render_template("index.html", login=True)
+
+@app.route("/login")
+def index():
+    return render_template("login.html")
+
+@app.route("/courses")
+def index():
+    return render_template("courses.html")
+
+@app.route("/register")
+def index():
+    return render_template("ergister.html")
