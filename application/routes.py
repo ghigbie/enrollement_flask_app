8c4cb21c3d8763c2856f0b2ea3985f0c75517ec7
ghigbie <gehigbie@gmail.com>
2020-05-15 19:34:20 -0700

    Add conditional rendering

--- application/routes.py
+++ application/routes.py
@@ -2,6 +2,7 @@ from application import app
 from flask import render_template
 
 @app.route("/")
+@app.route("/home")
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", login=True)
