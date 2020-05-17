b91a09a3f49b8effdaf6e06abfe2c2a18b6cd987
ghigbie <gehigbie@gmail.com>
2020-05-16 22:44:59 -0700

    Add users data model and route

--- application/routes.py
+++ application/routes.py
@@ -1,4 +1,4 @@
-from application import app
+from application import app, db
 from flask import render_template, request, Response, json
 
 course_data = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
@@ -45,3 +45,4 @@ def api(idx=None):
     
     return Response(json.dumps(jdata), mimetype="application/json")
 
+class User(db.Document):
