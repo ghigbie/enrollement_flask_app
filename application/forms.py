1d441364b6fd07d8a6e557e986a8e1781e08df52
ghigbie <gehigbie@gmail.com>
2020-05-18 21:55:49 -0700

    Edit forms and routes

--- application/forms.py
+++ application/forms.py
@@ -2,4 +2,19 @@ from flask_wtf import FlaskForm
 from wtfforms import StringField, PasswordField, SubmitField, BooleanField
 from wtfforms.validators import DataRequired
 
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = StringField("Password", validators=[DataRequired()])
+    remember_me = SubmitField("Remember Me")
+    submit = SubmitField("Login")
+
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = StringField("Password", validators=[DataRequired()])
+    password_confrim = StringField("Confirm Password", validators=[DataRequired()])
+    first_name = StringField("First Name", validators=[DataRequired()])
+    last_name = StringField("Last Name", validators=[DataRequired()])
+    submit = SubmitField("Register")
+
+
 
--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,7 @@
 from application import app, db
 from flask import render_template, request, Response, json
 from application.models import User, Course, Enrollment
+from application.forms import LoginForm, RegisterForm
 
 course_data = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
 
@@ -10,18 +11,20 @@ course_data = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP"
 def index():
     return render_template("index.html", login=True, index=True)
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html", login_nav=True)
+    form = LoginForm()
+    return render_template("login.html", form=form ,login_nav=True)
 
 @app.route("/courses")
 @app.route("/courses/<term>")
 def courses(term="2020"):
     return render_template("courses.html", course_data=course_data, courses=True, term=term)
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html", register=True)
+    form = RegisterForm()
+    return render_template("register.html", form=form, register=True)
 
 
 @app.route("/enrollment", methods=["GET", "POST"])
